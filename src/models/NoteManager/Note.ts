b367e222cfa1163efabf4d958d744e1906e96bf0
Hugo Fernández Solís <alu0101112664@ull.edu.es>
2021-05-07 15:18:21 +0100

    Specs de Note cumplidos

--- src/models/NoteManager/Note.ts
+++ src/models/NoteManager/Note.ts
@@ -12,47 +12,62 @@ export enum Color {
 
 /**
  * Implements a user note.
- * @var title Title of the note.
- * @var color Color of the note.
- * @var body Body of the note_
+ * @var title_ Title of the note.
+ * @var color_ Color of the note.
+ * @var body_ Body of the note_
  */
 export class Note {
   private title_: string;
   private color_: Color;
   private body_: string;
 
+  /**
+   * Initialices the values.
+   * @param newTitle Tittle of the note.
+   * @param newColor Color of the note.
+   * @param newBody Body of the note.
+   */
   constructor(newTitle: string, newColor: Color, newBody: string) {
     this.title_ = newTitle;
     this.color_ = newColor;
     this.body_ = newBody;
   }
 
+  public getJSON(): string {
+    let result: string = JSON.stringify(this);
+    result = result.replace(/{/g, "{\n\t");
+    result = result.replace(/:/g, ": ");
+    result = result.replace(/,/g, ",\n\t");
+    result = result.replace(/}/g, "\n}\n");
+    return result;
+  }
 
-  /* GETTERS */
+
+  /********************  GETTERS  ********************/
 
   /**
    * Returns the tittle of the note.
    */
-  public get title() {
+  public get title(): string {
     return this.title_;
   }
 
   /**
    * Returns the color of the note.
    */
-  public get color() {
+  public get color(): Color {
     return this.color_;
   }
 
   /**
    * Returns the body of the note.
    */
-  public get body() {
+  public get body(): string {
     return this.body_;
   }
 
 
-  /* SETTERS */
+  /******************** SETTERS ********************/
   
   /**
    * Sets a new note tittle.
